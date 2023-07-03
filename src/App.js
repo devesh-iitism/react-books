0b57d925eb6d966c3779c1a845931f3840a87fe2
Devesh Chaturvedi <dcdev001@gmail.com>
2023-07-03 16:34:22 +0530

    delete record

--- src/App.js
+++ src/App.js
@@ -26,7 +26,8 @@ function App() {
         setBooks(updatedBooks);
     };
 
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
         });
