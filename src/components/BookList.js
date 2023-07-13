802fb8fc6ac60884a5968aa90ef5194cee3dee8e
Devesh Chaturvedi <dcdev001@gmail.com>
2023-07-13 14:53:15 +0530

    refactor code

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,16 +2,14 @@ import BookShow from './BookShow';
 import { useContext } from "react";
 import BooksContext from '../context/books';
 
-function BookList({ books, onDelete, onEdit }) {
-    const {count, incrementCount} = useContext(BooksContext);
+function BookList() {
+    const { books } = useContext(BooksContext);
 
     const renderedBooks = books.map((book) => {
-        return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book}/>;
+        return <BookShow book={book}/>;
     });
     return (
         <div className="book-list">
-            {count} 
-            <button onClick={incrementCount}>Click</button>
             {renderedBooks}
         </div>
     );
